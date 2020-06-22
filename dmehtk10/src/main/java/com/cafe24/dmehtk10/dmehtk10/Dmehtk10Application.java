fb287794913a53f5fc9ae8b31b467165dff6b8f8
gd6 <gd6@DESKTOP-SDO16B8>
2020-06-22 11:41:47 +0900

    [정유석] cafe24 메인 페이지

---
+++ dmehtk10/src/main/java/com/cafe24/dmehtk10/dmehtk10/Dmehtk10Application.java
@@ -0,0 +1,13 @@
+package com.cafe24.dmehtk10.dmehtk10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dmehtk10Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dmehtk10Application.class, args);
+	}
+
+}
---
+++ dmehtk10/src/test/java/com/cafe24/dmehtk10/dmehtk10/Dmehtk10ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafe24.dmehtk10.dmehtk10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dmehtk10ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
